10183e52684983527a958a8fe75d44c3c8a75737
psterner <psterner@hotmail.com>
2023-07-28 20:38:17 +0200

    Make navMenu dropdown when clicking burger menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
 
 // initially the menu will not be visible
 let showMenu = false;
@@ -9,9 +11,13 @@ menuBtn.addEventListener("click", toggledMenu);
 function toggledMenu() {
   if (!showMenu) {
     hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
     showMenu = true;
   } else {
     hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
     showMenu = false;
   }
 }
